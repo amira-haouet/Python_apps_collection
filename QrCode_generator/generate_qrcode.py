3fe3283615aa6068568f6caf9f0d7613eaef85f1
amira-haouet <haouetamira@gmail.com>
2022-01-13 17:09:26 +0100

    Add Qr Code Generator

---
+++ QrCode_generator/generate_qrcode.py
@@ -0,0 +1,21 @@
+import qrcode
+
+#input_URL = "https://www.google.com/"
+input_URL1 = "https://github.com/amira-haouet"
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=15,
+    border=4,
+)
+
+#qr.add_data(input_URL)
+#qr.make(fit=True)
+
+qr.add_data(input_URL1)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("url_qrcode.png")
+
+print(qr.data_list)
