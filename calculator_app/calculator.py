67e8331a1783791d52ad4ed0fdd49d69fcb1a174
amira-haouet <haouetamira@gmail.com>
2022-01-16 20:38:13 +0100

    add functions

--- calculator_app/calculator.py
+++ calculator_app/calculator.py
@@ -1,10 +1,33 @@
-from tkinter import Tk,END
+from tkinter import Tk,END,Entry
 
 #click button
 def get_input(entry, argu):
     entry.insert(END, argu)
 
+#
+def backspace(entry):
+    input_len = len(entry.get())
+    entry.delete(input_len - 1)
 
 #clear function
 def clear(entry):
     entry.delete(0, END)
+
+#calcul funct 
+def calc(entry):
+    input_info = entry.get()
+    try:
+        output = str(eval(input_info.strip()))
+    except ZeroDivisionError:
+        popupmsg()
+        output = ""
+    clear(entry)
+    entry.insert(END, output)
+
+#
+def popupmsg():
+    popup = Tk()
+    popup.resizable(0, 0)
+    popup.geometry("120x100")
+    popup.title("Alert")
+ 
