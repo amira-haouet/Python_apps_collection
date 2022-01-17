2f722f86864c1f6d6b1b21b57ca63cd3defc19e8
amira-haouet <haouetamira@gmail.com>
2022-01-17 13:42:38 +0100

    add new app

---
+++ Tic_tac_toe/Tic_tac_toe ver2/README.md
@@ -0,0 +1,9 @@
+# Tic-Tac-Toe-AI
+
+
+
+## 3 modes:
+- Player vs. Player (2 - player mode)
+- Player vs. AI (1 - player mode)
+- AI(computer) vs. AI (*for fun*)
+
---
+++ Tic_tac_toe/Tic_tac_toe ver2/tic-tac-toe-AI.py
@@ -0,0 +1,11 @@
+####	TIC TAC TOE    ####
+
+#START;
+
+
+#FUNCTIONS;
+
+def default():
+	#To be printed as Default;
+    print("\nWelcome! Let's play TIC TAC TOE!\n")
+
