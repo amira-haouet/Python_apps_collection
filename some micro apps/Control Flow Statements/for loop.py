abacfb769a01b150efc3662952b9ffca70eaddb7
amira-haouet <haouetamira@gmail.com>
2022-01-22 22:54:41 +0100

    add new micro app

--- some micro apps/Control Flow Statements/for loop.py
+++ some micro apps/Control Flow Statements/for loop.py
@@ -11,3 +11,33 @@ for val in numbers:
     sum = sum+val
 
 print("The sum is", sum)
+print("  ")
+
+print(" ******************* ")
+# The range() function
+
+print(range(10))
+print("  ")
+
+print(" ******************* ")
+print(list(range(10)))
+print("  ")
+
+print(" ******************* ")
+print(list(range(2, 8)))
+print("  ")
+
+print(" ******************* ")
+print(list(range(2, 20, 3)))
+
+
+print("  ")
+
+print(" ******************* ")
+# Program to iterate through a list using indexing
+
+sports = ['foot', 'tennis', 'xxxx']
+
+# iterate over the list using index
+for i in range(len(sports)):
+    print("I like", sports[i])
