d92b412678a4db2b6e8252a006229493af8fd83f
amira-haouet <haouetamira@gmail.com>
2022-01-23 11:12:30 +0100

    add new micro app

--- some micro apps/POO/classes and objects.py
+++ some micro apps/POO/classes and objects.py
@@ -1,3 +1,8 @@
+print(" ")
+print("*********** Defining a Class in Python **************")
+print(" ")
+
+
 class Person:
     "This is a person class"
     age = 10
@@ -14,3 +19,36 @@ print(Person.greet)
 
 # Output: "This is a person class"
 print(Person.__doc__)
+
+print(" ")
+print("*********** Constructors in Python **************")
+print(" ")
+
+
+class ComplexNumber:
+    def __init__(self, r=0, i=0):
+        self.real = r
+        self.imag = i
+
+    def get_data(self):
+        print(f'{self.real}+{self.imag}j')
+
+
+# Create a new ComplexNumber object
+num1 = ComplexNumber(2, 3)
+
+# Call get_data() method
+# Output: 2+3j
+num1.get_data()
+
+# Create another ComplexNumber object
+# and create a new attribute 'attr'
+num2 = ComplexNumber(5)
+num2.attr = 10
+
+# Output: (5, 0, 10)
+print((num2.real, num2.imag, num2.attr))
+
+# but c1 object doesn't have attribute 'attr'
+# AttributeError: 'ComplexNumber' object has no attribute 'attr'
+print(num1.attr)
