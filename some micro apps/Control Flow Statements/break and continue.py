8b6697269c54108c31aa6b0baf5977245b863838
amira-haouet <haouetamira@gmail.com>
2022-01-23 10:50:22 +0100

    add new micro app

---
+++ some micro apps/Control Flow Statements/break and continue.py
@@ -0,0 +1,23 @@
+# Use of break statement inside the loop
+print(" ")
+
+print("Use of break statement inside the loop")
+for val in "hello you !":
+    if val == "o":
+        break
+    print(val)
+
+print("The end")
+
+print(" ")
+print(" Program to show the use of continue statement inside loops")
+
+
+# Program to show the use of continue statement inside loops
+
+for val in "hello you ":
+    if val == "o":
+        continue
+    print(val)
+
+print("The end")
